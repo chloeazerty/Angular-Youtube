8914fbadb61c955a9a97c3dc1e83388c502ce5ad
chloe azerty <chloe.rotaru@yahoo.com>
2022-06-26 08:52:48 +0200

    basic version

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() searchedValueSend: EventEmitter<string> = new EventEmitter();
+
+  inputSearched: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sendDataToParent(): void {
+    this.searchedValueSend.emit(this.inputSearched);
+  }
 }
